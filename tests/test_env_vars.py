9d6943d0b559c8647a72d92c69edddbe356fd861
Yaroslav Nikitenko <metst13@gmail.com>
2021-08-03 23:14:09 +0300

    Allow environmental variables in the config. Local removable media (with changing paths) and remotes with changing IPs can be used.

---
+++ tests/test_env_vars.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import os
+
+from yarsync import YARsync
+from yarsync.yarsync import _substitute_env
+from settings import TEST_DIR
+
+
+def test_env_vars():
+    os.environ["VAR"] = "var"
+    # variable substitution works for several lines
+    lines = "[]\npath=$VAR/maybe"
+    assert _substitute_env(lines).getvalue() == "[]\npath=var/maybe"
+    # unset variable is not substituted
+    lines = "[]\npath=$VAR1/maybe"
+    assert _substitute_env(lines).getvalue() == "[]\npath=$VAR1/maybe"
