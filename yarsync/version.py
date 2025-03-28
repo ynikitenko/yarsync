d49fbb547423d62b6af7feda80480afc521df927
Yaroslav Nikitenko <metst13@gmail.com>
2025-03-28 15:46:57 +0100

    Updates the generated manual.

--- yarsync/version.py
+++ yarsync/version.py
@@ -1,3 +1,5 @@
-# don't forget to update the version in docs/source/yarsync.1.md manually
+# don't forget to update the version in docs/source/yarsync.1.md manually,
+# as well as copyright years in Sphinx conf
+# and then to re-generate the man file with 'make man'
 
 __version__ = '0.3'
